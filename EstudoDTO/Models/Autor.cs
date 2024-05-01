58f15ec93644575e6aad046969ce2f7e4864cfe5
Daniel <danie@DESKTOP-9JEO5TN>
2024-05-01 11:20:56 -0300

    Add project files.

---
+++ EstudoDTO/Models/Autor.cs
@@ -0,0 +1,8 @@
+﻿namespace EstudoDTO.Models
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ EstudoDTO/Models/Livro.cs
@@ -0,0 +1,16 @@
+﻿namespace EstudoDTO.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public required string Titulo { get; set; }
+        public int Ano { get; set; }
+        public decimal Preco { get; set; }
+        public required string Genero { get; set; }
+
+        // Foreign Key
+        public int AutorId { get; set; }
+        // Navigation property
+        public required Autor Autor { get; set; }
+    }
+}
